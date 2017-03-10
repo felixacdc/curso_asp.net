ee3cb08a5abf11e81faf5eade1b0369210984462
felixacdc <pfmata01@gmail.com>
2017-03-10 16:56:54 -0600

    [modify] Tutorial 4 parte 3

---
+++ CapaAccesoDatos/EmpleadoDAO.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CapaEntidades;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace CapaAccesoDatos
+{
+    class EmpleadoDAO
+    {
+        #region "PATRON SINGLETON"
+        private static EmpleadoDAO daoEmpleado = null;
+        private EmpleadoDAO() { }
+        public static EmpleadoDAO getInstance()
+        {
+            if(daoEmpleado == null)
+            {
+                daoEmpleado = new EmpleadoDAO();
+            }
+
+            return daoEmpleado;
+        }
+        #endregion
+
+        public Empleado AccesoSistema(String Usuario, String Password)
+        {
+            SqlConnection conexion = null;
+            SqlCommand cmd = null;
+            Empleado objEmpleado = null;
+            SqlDataReader dr = null;
+            
+            try
+            {
+                conexion = Conexion.getInstance().conexionBD();
+                cmd = new SqlCommand("spAccesoSistema", conexion);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@prmUser", Usuario);
+                cmd.Parameters.AddWithValue("@prmPass", Password);
+                dr = cmd.ExecuteReader();
+
+                if (dr.Read())
+                {
+
+                }
+            } catch (Exception e)
+            {
+
+            } finally
+            {
+
+            }
+        }
+    }
+}
--- CapaEntidades/TipoEmpleado.cs
+++ CapaEntidades/TipoEmpleado.cs
@@ -8,5 +8,20 @@ namespace CapaEntidades
 {
     class TipoEmpleado
     {
+        public int ID { get; set; }
+        public String Descripcion { get; set; }
+        public bool Estado { get; set; }
+
+        public TipoEmpleado()
+        {
+
+        }
+
+        public TipoEmpleado(int ID, String Descripcion, bool Estado)
+        {
+            this.ID = ID;
+            this.Descripcion = Descripcion;
+            this.Estado = Estado;
+        }
     }
 }
